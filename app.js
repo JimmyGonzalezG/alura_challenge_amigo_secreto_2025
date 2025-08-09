52d0816fa4fd7c113384f88ca88074ccf5dea8ce
JimmyGonzalezG <j76gonzalez@gmail.com>
2025-08-08 21:49:43 -0500

    Adelantado el mensaje de commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 let amigos=[];
 
 
-function addFriend() {
+function agregarAmigo() {
     let nombre = document.getElementById("amigo").value.trim();
     if (nombre === "") {
         alert("Por favor, inserte un nombre.");
@@ -13,13 +13,45 @@ function addFriend() {
             return;
         } else{
         amigos.push(nombre);
-        ingresarNombre.value = ""; // Limpiar el campo de entrada
+        nombre.value = ""; // Limpiar el campo de entrada
         console.log(`Tienes ${amigos.length} amigos.`);
         console.log("Amigo agregado:", nombre);
+        actualizarLista();
+        limpiarEntrada();        
+        }
+        }
+    }   
+
+
+    function limpiarEntrada() {
+        document.getElementById("amigo").value = ""; // Limpiar el campo de entrada
+    }
+
+
+    function actualizarLista() {
+        let lista = document.getElementById("listaAmigos");
+        lista.innerHTML = ""; // Limpiar la lista actual
+        amigos.forEach(function(amigo) {
+            let li = document.createElement("li");
+            li.textContent = amigo;
+            lista.appendChild(li);
+        });
+    }
+
+    function sortearAmigo() {
+        if (amigos.length === 0) {
+            alert("No hay amigos en la lista para sortear.");
+            return;
+        }else{
+            let aleatorioAmigo = amigos[Math.floor(Math.random() * amigos.length)];
+            console.log(amigos);
+            console.log(aleatorioAmigo);
+            console.log("Amigo sorteado:", aleatorioAmigo);
+            let resultado = document.getElementById("resultado");
+            resultado.textContent = `El amigo sorteado es: ${aleatorioAmigo}`;
         }
     }
 
 
    
 
-}
